ade903991b578b467bb3327088390bc08b175acf
Andrew Vasquez <andrew.vasquez@infiniteblue.com>
2023-10-10 11:56:18 -0700

    added tmux navigator and vimbegood

--- init.lua
+++ init.lua
@@ -147,18 +147,30 @@ require('lazy').setup({
   {
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
+    config = function()
+        require('ibl').setup {
+            indent = {
+                char = '┊',
+            },
+        }
+    end,
   },
 
   -- "gc" to comment visual regions/lines
   {
     'numToStr/Comment.nvim',
     opts = {},
+    hooks = {
+      before_comment = {
+        jsx = function()
+          require('vim.treesitter.query').set_query('javascriptreact', 'comments', '(comment) @comment')
+          return true
+        end
+      }
+    }
   },
 
   -- Fuzzy Finder (files, lsp, etc)
